a2c6544539682f08a42a5c87d0ec6f58fe398202
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-08-10 21:57:16 -0300

    Improving Loginscreen test

--- frontend/src/components/UnsignedInPage/InputArea/Login/login.spec.tsx
+++ frontend/src/components/UnsignedInPage/InputArea/Login/login.spec.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import { Login } from 'src/pages/UnsignedInPage';
+import { Login } from './index';
 
-it('render correctly', () => {
-  render(<Login />);
+describe('Login page', () => {
+  it('render correctly', () => {
+    render(<Login />);
 
-  const test = screen.getByText('Forget my password');
+    const test = screen.getByText('Forget my password');
 
-  expect(test).toBeInTheDocument();
+    expect(test).toBeInTheDocument();
+  });
 });
