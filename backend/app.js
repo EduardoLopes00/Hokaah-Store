d89ab27d2dff7781552fdea96d1cb875ecd4a9e2
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-09-05 22:13:07 -0300

    Adding CORS config to the backend

--- backend/app.js
+++ backend/app.js
@@ -1,108 +1,112 @@
-require("dotenv").config()
-require("./config/database").connect()
-const express = require('express')
-const User = require("./src/models/user")
-const bycript = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const auth = require("./middleware/auth")
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const User = require("./src/models/user");
+const bycript = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("./middleware/auth");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const isAllInputFilled = (reqBody) => {
-    return (reqBody.first_name && reqBody.last_name && reqBody.email && reqBody.password)    
-}
+  return (
+    reqBody.first_name && reqBody.last_name && reqBody.email && reqBody.password
+  );
+};
 
 const isValidUser = (user) => {
-    return !(!user)
-}
+  return !!user;
+};
 
 const isValidPassword = async (user, password) => {
-    return await bycript.compare(password, user.password)
-}
+  return await bycript.compare(password, user.password);
+};
 
-app.post('/register', async (req, res) => {
-    
-    try {
-        console.log('req: ', req.body)
-        
-        if (!isAllInputFilled(req.body)) return res.status(400).send("All input is required")
-        
-        const { first_name, last_name, email, password } = req.body;
+app.post("/register", async (req, res) => {
+  try {
+    console.log("req: ", req.body);
 
-        const oldUser = await User.findOne({email})
+    if (!isAllInputFilled(req.body))
+      return res.status(400).send("All input is required");
 
-        if (oldUser) {
-            return res.status(409).send("User already registred!");
-        }
+    const { first_name, last_name, email, password } = req.body;
 
-        const encryptedPassword = await bycript.hash(password, 10)
+    const oldUser = await User.findOne({ email });
 
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(),
-            password: encryptedPassword
-        })
-
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "2h"
-            }
-        )
-
-        user.token = token;
-
-        res.status(201).json(user)
-    } catch (err) {
-        console.log(err)
-
-        res.status(500)
-            .send({message: "Something went wrong when trying to register",
-                     error: err})
-        
-    }
-    
-})
-
-app.post('/login', async (req, res) => {
-
-    try {
-        const { email, password } = req.body;
-
-        if (!(email && password)) {
-            res.status(400).send("All input is required");
-        }
-
-        const user = await User.findOne({email});
-      
-        if (!isValidUser(user) || (!isValidPassword(user, password))) {
-            res.status(400).send("Invalid Credentials")            
-        }
-
-        const token = jwt.sign(
-            {user_id: user._id, email},
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "2h"
-            }
-        )
-
-        user.token = token
-
-        res.status(200).json(user);
-    } catch (err) {
-        console.log(err)
-        res.status(500).send({message: "Something went wrong when trying to log in.", err: err})
+    if (oldUser) {
+      return res.status(409).send("User already registred!");
     }
 
-})
+    const encryptedPassword = await bycript.hash(password, 10);
 
-app.post('/welcome', auth, (req, res) => {
-    res.status(200).send("Welcome")
-})
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
 
-module.exports= app
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).send({
+      message: "Something went wrong when trying to register",
+      error: err,
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!isValidUser(user) || !isValidPassword(user, password)) {
+      res.status(400).send("Invalid Credentials");
+    }
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    user.token = token;
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Something went wrong when trying to log in.",
+      err: err,
+    });
+  }
+});
+
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome");
+});
+
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const app = require('./app')
-const http = require('http')
-const server = http.createServer(app)
+const app = require("./app");
+const http = require("http");
+const server = http.createServer(app);
 
-const port = process.env.API_PORT || 5000
+const port = process.env.API_PORT || 5000;
 
 server.listen(port, () => {
-    console.log(`Server is running in the port ${port}`)
-})
+  console.log(`Server is running in the port ${port}`);
+});
