3099bb6ab17153178c7cd782e5b429f2e7c70a8f
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-09-07 22:54:58 -0300

    feat: Add and configuring react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import './App.scss';
 import { Router } from 'src/routes';
 import { AuthContextProvider } from './contexts/Auth';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <AuthContextProvider>
-        <Router />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Router />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </div>
   );
 }
