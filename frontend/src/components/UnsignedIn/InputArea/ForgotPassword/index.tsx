d515f7ff9255e729b1a5ae3829402b2cac0300fd
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-01-24 10:23:55 -0300

    feat: Build recover password interface

--- frontend/src/components/UnsignedIn/InputArea/ForgotPassword/index.tsx
+++ frontend/src/components/UnsignedIn/InputArea/ForgotPassword/index.tsx
@@ -1,3 +1,47 @@
+import './styles.scss';
+import { TextButton } from 'src/components/General/TextButton';
+import { InputComponent } from 'src/components/General/InputComponent';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const ForgotPassword = () => {
-  return <h1>Recover password</h1>;
+  const [recoverEmail, setRecoverEmail] = useState('');
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="input-area-content">
+      <h1 className="usignedInPage-title">Sign In</h1>
+
+      <p className="descriptive-text">
+        Type your e-mail below and a code will be sent to recover your password
+      </p>
+
+      <div className="fields-area">
+        <InputComponent
+          value={recoverEmail}
+          onChange={(event) => {
+            setRecoverEmail(event.target.value);
+          }}
+          name="email"
+          type="email"
+          labelText="E-mail"
+        />
+
+        <TextButton text="Send" />
+
+        <p className="remeber-password-text">
+          Remember de password?{' '}
+          <span
+            className="link-text"
+            onClick={() => {
+              navigate('/signin');
+            }}>
+            {' '}
+            Sign in Now!
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
