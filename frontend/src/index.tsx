1f8c902415295358427812604b89bc4bc5a3da07
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2023-04-15 20:05:53 -0300

    feat: remove all redux files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './redux/store';
 import './styles/global.scss';
 import 'src/styles/theme.scss';
 
@@ -13,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/redux/store.ts
+++
@@ -1,4 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { loginReducer } from 'src/redux/reducers/loginReducer';
-
-export const store = configureStore({ reducer: loginReducer });
